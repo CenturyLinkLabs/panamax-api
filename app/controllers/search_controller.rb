f9608dfde9e2374b2fb47c7b35869849a95696c7
Mike Arnold <dharmamike@gmail.com>
2014-03-27 13:39:48 -0500

    adding local images to search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,6 +5,7 @@ class SearchController < ApplicationController
   def index
     search_results = {}
     search_results[:remote_images] = Docker::Image.search(query={ term: params[:q] })
+    search_results[:local_images] = Docker::Image.all
     respond_with search_results
   end
 
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -5,19 +5,40 @@ describe SearchController do
   describe 'GET index' do
 
     let(:query) { 'some, search, query' }
-    let(:fake_image_data) { [{fake_image_data: 'bla'}] }
+    let(:fake_image_data) { [{'fake_image_data' =>'bla', 'Id' => "fake"}] }
+    let(:connection) do
+      conn = Docker::Connection.new('unix:///var/run/docker.sock', {})
+      conn.stub(get: (fake_image_data.to_json))
+      conn
+    end
+
+    before { Docker.stub(:connection).and_return(connection) }
 
     it 'queries the docker index with the q argument' do
       expect(Docker::Image).to receive(:search).with({ term: query })
-      get :index, {q: "#{query}", format: 'json'}
+      get :index, { q: "#{query}", format: 'json' }
     end
 
-    it 'returns an array of image data' do
-      Docker::Image.stub(:search).and_return(fake_image_data)
-      get :index, {q: "#{query}", format: 'json'}
-
-      expect(response.body).to eq({remote_images: fake_image_data}.to_json)
+    it 'retrieves all local images through the Docker remote api' do
+      expect(Docker::Image).to receive(:all)
+      get :index, { q: "#{ query}", format: 'json' }
     end
+
+    context 'returns an array of image data' do
+      it 'includes Docker index image data' do
+        Docker::Image.stub(:search).and_return(fake_image_data)
+        get :index, { q: "#{ query}", format: 'json' }
+
+        expect(JSON.parse(response.body)).to include({'remote_images' => fake_image_data})
+      end
+
+      it 'includes local image data' do
+        Docker::Image.stub(:all).and_return(fake_image_data)
+        get :index, { q: "#{query}", format: 'json' }
+        expect(JSON.parse(response.body)).to include({'local_images' => fake_image_data})
+      end
+    end
+
   end
 
 end
