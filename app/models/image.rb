0ce757cfd72df085e14f79645701b3656f5095d4
Brian DeHamer <brian.dehamer@hp.com>
2014-03-28 12:27:04 -0700

    Expose Docker API methods on Image model
    [68362906]

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,26 @@
 class Image < ActiveRecord::Base
+
+  attr_accessor :is_official
+  attr_accessor :is_trusted
+  attr_accessor :star_count
+
   has_and_belongs_to_many :templates
+
+  def self.search_remote_index(query={})
+    images = Docker::Image.search(query)
+    images.map do |image|
+      new(
+        repository: image.id,
+        description: image.info['description'],
+        is_official: image.info['is_official'],
+        is_trusted: image.info['is_trusted'])
+    end
+  end
+
+  def self.all_local
+    images = Docker::Image.all
+    images.map do |image|
+      new(repository: image.info['RepoTags'].first)
+    end
+  end
 end
