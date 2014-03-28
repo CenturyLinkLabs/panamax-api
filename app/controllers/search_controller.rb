569e6323dddf0817afe62e4cf6666aedb53eee16
Mike Arnold <dharmamike@gmail.com>
2014-03-27 20:59:10 -0500

    filtering local images by repo (name) for search results based on query term

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,12 @@ class SearchController < ApplicationController
   def index
     search_results = {}
     search_results[:remote_images] = Docker::Image.search(query={ term: params[:q] })
-    search_results[:local_images] = Docker::Image.all
+    search_results[:local_images] = images_with_repository(params[:q])
     respond_with search_results
   end
 
+  private
+  def images_with_repository(repo)
+    Docker::Image.all.map { |image| image if image.info['Repository'] =~ /#{params[:q]}/ }.compact
+  end
 end
