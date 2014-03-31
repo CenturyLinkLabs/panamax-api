c39cbc5bf7c45dda4cba2997487a053e20f93704
Mike Arnold <dharmamike@gmail.com>
2014-03-31 11:41:05 -0500

    adding a named scope to Template model for querying the db with name 'LIKE' some search criteria

--- app/models/template.rb
+++ app/models/template.rb
@@ -2,5 +2,6 @@ class Template < ActiveRecord::Base
   has_and_belongs_to_many :images
 
   scope :recommended, -> { where(recommended: true) }
+  scope :named_like, -> (name){ where("name LIKE ?", "%#{name}%") }
 
 end
