c878c8eb3ded3bcff6adccdd4576326796125973
Mike Arnold <dharmamike@gmail.com>
2014-04-02 16:46:27 -0500

    telling coveralls that ours is a rails app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'coveralls'
 
-Coveralls.wear!
+Coveralls.wear!('rails')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
