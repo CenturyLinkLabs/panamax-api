1a478d0dfd52ca80ea915bd05c0f152dd538ad94
Mike Arnold <dharmamike@gmail.com>
2014-04-04 15:01:48 -0500

    adding tests for template endpoint

---
+++ spec/controllers/templates_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe TemplatesController do
+
+  describe 'GET templates' do
+
+    it "returns the list of templates" do
+      get :index, format: :json
+      expect(response.body).to include(TemplateSerializer.new(Template.first).to_json)
+    end
+
+    it "returns a specific template" do
+      get :show, { id: Template.first.id, format: :json }
+      expect(response.body).to eq TemplateSerializer.new(Template.first).to_json
+    end
+
+  end
+
+end
