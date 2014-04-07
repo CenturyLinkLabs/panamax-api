2729dae81bdcfb8a98d17d8516abef6790b56251
Mike Arnold <dharmamike@gmail.com>
2014-04-07 10:51:38 -0500

    adding services as a nested resource for apps

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,16 @@
+class ServicesController < ApplicationController
+  respond_to :json
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+end
---
+++ spec/routing/apps_routes_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+
+describe 'apps routes' do
+
+  it 'routes get to the apps controller index' do
+    expect(get: '/apps').to route_to(
+        controller: 'apps',
+        action: 'index'
+    )
+  end
+
+  it 'has a named route for the apps controller index' do
+    expect(get: apps_path).to route_to(
+                                          controller: 'apps',
+                                          action: 'index'
+                                      )
+  end
+
+  it 'routes get with an id to a the apps controller show action' do
+    expect(get: '/apps/1').to route_to(
+                                        controller: 'apps',
+                                        action: 'show',
+                                        id: '1'
+                                    )
+  end
+
+  it 'has a named route for the apps controller show action' do
+    expect(get: app_path(1)).to route_to(
+                                            controller: 'apps',
+                                            action: 'show',
+                                            id: '1'
+                                        )
+  end
+
+  it 'routes post to the apps controller create action' do
+    expect(post: '/apps').to route_to(
+        controller: 'apps',
+        action: 'create'
+                                     )
+  end
+
+  describe 'services sub-resource routes' do
+    it 'routes get to the app services controller index' do
+      expect(get: '/apps/1/services').to route_to(
+                                  controller: 'services',
+                                  action: 'index',
+                                  app_id: '1'
+                              )
+    end
+
+    it 'has a named route for the app services controller index' do
+      expect(get: app_services_path(1)).to route_to(
+                                    controller: 'services',
+                                    action: 'index',
+                                    app_id: '1'
+                                )
+    end
+
+    it 'routes get with an id to a the app services controller show action' do
+      expect(get: '/apps/1/services/1').to route_to(
+                                    controller: 'services',
+                                    action: 'show',
+                                    app_id: '1',
+                                    id: '1'
+                                )
+    end
+
+    it 'has a named route for the apps controller show action' do
+      expect(get: app_service_path(1, 2)).to route_to(
+                                      controller: 'services',
+                                      action: 'show',
+                                      app_id: '1',
+                                      id: '2'
+                                  )
+    end
+
+    it 'routes post to the app services controller create action' do
+      expect(post: '/apps/1/services').to route_to(
+                                   controller: 'services',
+                                   action: 'create',
+                                   app_id: '1'
+                               )
+    end
+
+
+  end
+end
