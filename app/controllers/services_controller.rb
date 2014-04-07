60a432e76aa58c9f7cd5edd87c2f18db98920393
Mike Arnold <dharmamike@gmail.com>
2014-04-07 15:21:46 -0500

    implementing GET app services endpoints

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,16 +1,23 @@
 class ServicesController < ApplicationController
   respond_to :json
 
-  def index
+  before_filter :app
 
+  def index
+    respond_with @app.services
   end
 
   def show
-
+    respond_with @app.services.find(params[:id])
   end
 
   def create
 
+  end
+
+  private
+  def app
+    @app ||= App.find(params[:app_id])
   end
 
 end
