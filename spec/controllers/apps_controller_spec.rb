f3e7982122e512ea4548108fb22cd55096791dae
Mike Arnold <dharmamike@gmail.com>
2014-04-08 10:29:27 -0500

    rewording spec

--- spec/controllers/apps_controller_spec.rb
+++ spec/controllers/apps_controller_spec.rb
@@ -2,19 +2,22 @@ require 'spec_helper'
 
 describe AppsController do
 
-  describe 'GET index' do
+  describe '#index' do
 
     it 'returns an array' do
       get :index, format: :json
       expect(JSON.parse(response.body)).to be_an Array
     end
 
+  end
+
+  describe '#show' do
     it "returns a specific app" do
       get :show, { id: App.first.id, format: :json }
       expect(response.body).to eq (AppSerializer.new(App.first)).to_json
     end
 
-    it 'app representations have an array of services in the services attribute' do
+    it 'returns app representations with an array of services in the services attribute' do
       get :show, { id: App.first.id, format: :json }
       expect(JSON.parse(response.body)).to have_key('services')
       expect(JSON.parse(response.body)['services']).to be_an Array
--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -1,22 +1,25 @@
 require 'spec_helper'
 
 describe ServicesController do
+  let(:app){ App.first }
 
-  describe 'GET index' do
-
-    let(:app){ App.first }
+  describe '#index' do
 
     it 'returns an array' do
       get :index, {app_id: app.id, format: :json}
       expect(JSON.parse(response.body)).to be_an Array
     end
 
+  end
+
+  describe '#show' do
+
     it "returns a specific app" do
       get :show, { app_id: app.id, id: Service.first.id, format: :json }
       expect(response.body).to eq (ServiceSerializer.new(Service.first)).to_json
     end
 
-    it 'service representations have an app representation in the app attribute' do
+    it 'returns service representations with an app representation in the app attribute' do
       get :show, { id: Service.first.id, app_id: app.id, format: :json }
       expect(JSON.parse(response.body)).to have_key('app')
       expect(JSON.parse(response.body)['app']).to eq JSON.parse(AppLiteSerializer.new(app).to_json)
