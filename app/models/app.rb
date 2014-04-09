d79dd91dbec0afede9f3f7926f2e25d1ad5995f0
Mike Arnold <dharmamike@gmail.com>
2014-04-09 08:58:52 -0500

    applications can be created from a template

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,7 @@
 class App < ActiveRecord::Base
   has_many :services
+
+  def self.create_from_template(t)
+    self.create(name: t.name, from: "Template: #{t.name}")
+  end
 end
--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe App do
   it { should have_many(:services) }
+
+  describe '.create_from_template' do
+    it 'creates a new app using values from the template' do
+      template = Template.where(name: 'wordpress').first
+      expect(App).to receive(:create).with(name: template.name, from: "Template: #{template.name}").and_return(true)
+      App.create_from_template(template)
+    end
+
+  end
 end
