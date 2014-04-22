3d2a2e3ef2120f05c5e0131ce7faf9d911259acc
Brian DeHamer <brian.dehamer@savvis.com>
2014-04-22 11:40:44 -0700

    Ensure API version is passed to registry API
    [#69949976]

--- lib/panamax_agent/registry/client.rb
+++ lib/panamax_agent/registry/client.rb
@@ -4,8 +4,20 @@ require 'panamax_agent/registry/client/repositories'
 module PanamaxAgent
   module Registry
     class Client < PanamaxAgent::Client
+
+      def initialize(options={})
+        super
+        @base_path = registry_api_version
+      end
+
       include PanamaxAgent::Registry::Connection
       include PanamaxAgent::Registry::Client::Repositories
+
+      protected
+
+      def resource_path(resource, *parts)
+        parts.unshift(resource).unshift(@base_path).join('/')
+      end
     end
   end
 end
