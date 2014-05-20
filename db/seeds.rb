dc7fa29915a9abe03d255a94d3afa86e721ee346
Mike Arnold <dharmamike@gmail.com>
2014-05-20 11:29:41 -0500

    preventing template create if a template with that name already exists [finishes #71508554]

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,69 +6,75 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-wp = Template.create(
-  name: 'Wordpress',
-  description: 'This is a wordpress template',
-  recommended: true,
-  icon: 'http://panamax.ca.tier3.io/template_logos/wordpress.png',
-  documentation: "# Wordpress\r\nThis is a WordPress template based on the [panamax/panamax-docker-wordpress](https://index.docker.io/u/panamax/panamax-docker-wordpress/) and [panamax/panamax-docker-mysql](https://index.docker.io/u/panamax/panamax-docker-mysql/) images.\r\nThe template exposes port 80 in the container to port 8080 on the Docker host.  In order to view the WordPress admin console after running the template, ensure that the virtual machine serving as the Docker host is forwarding a port to port 8080 on the host."
-)
-web_cat = TemplateCategory.create(
-  name: 'Web Tier',
-  template: wp
-)
-wp.images.create(
-  name: 'DB_1',
-  repository: 'panamax/panamax-docker-mysql',
-  tag: 'latest',
-  description: 'MySQL',
-  expose: [3306],
-  environment: { 'MYSQL_ROOT_PASSWORD' => 'pass@word01'},
-  ports: [{host_port: 3306, container_port: 3306}],
-  icon: 'http://panamax.ca.tier3.io/service_icons/icon_service_db_grey.png'
-)
-wp.images.create(
-    name: 'WP',
-    repository: 'panamax/panamax-docker-wordpress',
-    tag: 'latest',
-    description: 'Wordpress',
-    links: [{service: 'DB_1', alias:'DB_1'}],
-    ports: [{host_port: 8080, container_port: 80}],
-    expose: [80],
-    environment: { 'DB_PASSWORD' => 'pass@word01' },
-    categories: [web_cat],
-    icon: 'http://panamax.ca.tier3.io/service_icons/icon_service_wp_grey.png'
-)
-
-rails = Template.create(
-    name: 'Rails',
-    description: 'This is a Rails app template',
+unless Template.where(name: 'Wordpress').present?
+  wp = Template.create(
+    name: 'Wordpress',
+    description: 'This is a wordpress template',
     recommended: true,
-    icon: 'http://panamax.ca.tier3.io/template_logos/default.png'
-
-)
-rails.images.create(
-    name: 'DB_2',
-    repository: 'dharmamike/dc-pgsql',
+    icon: 'http://panamax.ca.tier3.io/template_logos/wordpress.png',
+    documentation: "# Wordpress\r\nThis is a WordPress template based on the [panamax/panamax-docker-wordpress](https://index.docker.io/u/panamax/panamax-docker-wordpress/) and [panamax/panamax-docker-mysql](https://index.docker.io/u/panamax/panamax-docker-mysql/) images.\r\nThe template exposes port 80 in the container to port 8080 on the Docker host.  In order to view the WordPress admin console after running the template, ensure that the virtual machine serving as the Docker host is forwarding a port to port 8080 on the host."
+  )
+  web_cat = TemplateCategory.create(
+    name: 'Web Tier',
+    template: wp
+  )
+  wp.images.create(
+    name: 'DB_1',
+    repository: 'panamax/panamax-docker-mysql',
     tag: 'latest',
-    description: 'PostgreSQL',
-    ports: [{host_port: 5432, container_port: 5432}],
+    description: 'MySQL',
+    expose: [3306],
+    environment: { 'MYSQL_ROOT_PASSWORD' => 'pass@word01'},
+    ports: [{host_port: 3306, container_port: 3306}],
     icon: 'http://panamax.ca.tier3.io/service_icons/icon_service_db_grey.png'
-)
-rails.images.create(
-  name: 'APP',
-  repository: 'dharmamike/dc-rails',
-  tag: 'latest',
-  description: 'welcome to rails',
-  links: [{service: 'DB_2', alias:'DB_1'}],
-  ports: [{host_port: 8088, container_port: 3000}],
-  icon: 'http://panamax.ca.tier3.io/service_icons/icon_service_docker_grey.png'
+  )
+  wp.images.create(
+      name: 'WP',
+      repository: 'panamax/panamax-docker-wordpress',
+      tag: 'latest',
+      description: 'Wordpress',
+      links: [{service: 'DB_1', alias:'DB_1'}],
+      ports: [{host_port: 8080, container_port: 80}],
+      expose: [80],
+      environment: { 'DB_PASSWORD' => 'pass@word01' },
+      categories: [web_cat],
+      icon: 'http://panamax.ca.tier3.io/service_icons/icon_service_wp_grey.png'
+  )
+end
 
-)
+unless Template.where(name: 'Rails').present?
+  rails = Template.create(
+      name: 'Rails',
+      description: 'This is a Rails app template',
+      recommended: true,
+      icon: 'http://panamax.ca.tier3.io/template_logos/default.png'
 
-Template.create(
-  name: 'Apache',
-  description: 'This is a recommended apache template',
-  recommended: false,
+  )
+  rails.images.create(
+      name: 'DB_2',
+      repository: 'dharmamike/dc-pgsql',
+      tag: 'latest',
+      description: 'PostgreSQL',
+      ports: [{host_port: 5432, container_port: 5432}],
+      icon: 'http://panamax.ca.tier3.io/service_icons/icon_service_db_grey.png'
+  )
+  rails.images.create(
+    name: 'APP',
+    repository: 'dharmamike/dc-rails',
+    tag: 'latest',
+    description: 'welcome to rails',
+    links: [{service: 'DB_2', alias:'DB_1'}],
+    ports: [{host_port: 8088, container_port: 3000}],
+    icon: 'http://panamax.ca.tier3.io/service_icons/icon_service_docker_grey.png'
 
-)
+  )
+end
+
+unless Template.where(name: 'Apache')
+  Template.create(
+    name: 'Apache',
+    description: 'This is a recommended apache template',
+    recommended: false,
+
+  )
+end
