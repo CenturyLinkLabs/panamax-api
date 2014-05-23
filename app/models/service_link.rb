5932bb10575ada5e960cb02c03e038f479b5351f
Alex Welch <me@alexwelch.com>
2014-05-23 08:29:41 -0600

    Do not allow linking a service to itself. [#71774854]

--- app/models/service_link.rb
+++ app/models/service_link.rb
@@ -3,8 +3,20 @@ class ServiceLink < ActiveRecord::Base
 
   validates :linked_to_service_id, uniqueness: { scope: :linked_from_service_id }
   validates :alias, presence: true
+  validate :cannot_link_to_self
 
   def link_string
     "#{linked_to_service.name}:#{self.alias}"
+  end
+
+  private
+
+  def cannot_link_to_self
+    if linked_to_service_id.present? &&
+      linked_from_service_id.present? &&
+      linked_to_service_id == linked_from_service_id
+
+      errors.add(:base, "can't link to self")
+    end
   end
 end
--- spec/models/service_link_spec.rb
+++ spec/models/service_link_spec.rb
@@ -3,8 +3,28 @@ require 'spec_helper'
 describe ServiceLink do
   it { should belong_to(:linked_to_service) }
 
-  it { should validate_presence_of :alias }
-  it { should validate_uniqueness_of(:linked_to_service_id).scoped_to(:linked_from_service_id) }
+  describe 'validations' do
+    it { should validate_presence_of :alias }
+    it { should validate_uniqueness_of(:linked_to_service_id).scoped_to(:linked_from_service_id) }
+
+    describe 'linking to itself' do
+      before do
+        subject.alias = 'alias'
+      end
+
+      it 'errors when linked to and from ids match' do
+        subject.linked_from_service_id = 3
+        subject.linked_to_service_id = 3
+        expect(subject.valid?).to be_false
+        expect(subject.errors[:base]).to include "can't link to self"
+      end
+
+      it 'does not error if one of the ids is nil' do
+        subject.linked_to_service_id = 3
+        expect(subject.valid?).to be_true
+      end
+    end
+  end
 
   describe '#link_string' do
     let(:linked_to_service) { Service.new(name: 'mysql') }
