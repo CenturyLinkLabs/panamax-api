3d4d47ccf762c8098292554c88d21cb7ac945577
Brian DeHamer <brian@dehamer.com>
2014-05-27 10:09:17 -0700

    Validations for ServiceCategory [#71967486]

--- spec/models/service_category_spec.rb
+++ spec/models/service_category_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe ServiceCategory do
+
+  it { should validate_presence_of(:app_category_id) }
+
+  it do
+    should validate_numericality_of(:position).
+      only_integer.
+      is_greater_than_or_equal_to(0)
+  end
+
   it { should delegate_method(:name).to(:app_category) }
 end
