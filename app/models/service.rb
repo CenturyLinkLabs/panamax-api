8b01c483068e3212c116735cb61519d27eaa1699
Brian DeHamer <brian@dehamer.com>
2014-05-26 18:48:20 -0700

    Update categories as part of service update [#71967486]

--- app/models/service.rb
+++ app/models/service.rb
@@ -56,7 +56,7 @@ class Service < ActiveRecord::Base
 
   def copy_categories_from_image(image, app_categories)
     image.categories.each do |image_cat|
-      app_category = app_categories.find { |app_cat| app_cat.name == image_cat.name }
+      app_category = app_categories.detect { |app_cat| app_cat.name == image_cat.name }
       self.categories << ServiceCategory.new(
         app_category_id: app_category.id,
         position: image_cat[:position])
@@ -80,10 +80,16 @@ class Service < ActiveRecord::Base
       )
     end
 
+    attributes[:categories] ||= []
+    attributes[:categories].map! do |category|
+      self.categories.find_or_initialize_by(
+        app_category_id: category[:id],
+        position: category[:position]
+      )
+    end
+
     attributes[:ports] ||= []
     attributes[:environment] ||= {}
-
-    # Do same as above w/ categories here
 
     self.update(attributes)
   end
