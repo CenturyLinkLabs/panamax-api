fd9dc284abe371ec7d27d6c425d5c7e7a696f166
Alex Welch <me@alexwelch.com>
2014-05-29 12:28:10 -0600

    Adds ability to search templates only. [#72273968]

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,12 +3,27 @@ class SearchController < ApplicationController
   respond_to :json
 
   def index
-    q = params[:q]
-    search_results = {q: q}
-    search_results[:remote_images] = Image.search_remote_index(term: q)
-    search_results[:local_images] = Image.local_with_repo_like(q)
-    search_results[:templates] = Template.recommended.named_like(q)
-    respond_with search_results
+    q, type = search_params[:q], search_params[:type]
+    respond_with perform_search(q, type).merge(q: q)
+  end
+
+  private
+
+  def perform_search(q, type)
+    {}.tap do |results|
+      case type
+      when 'template'
+        results[:templates] = Template.recommended.named_like(q)
+      else
+        results[:remote_images] = Image.search_remote_index(term: q)
+        results[:local_images] = Image.local_with_repo_like(q)
+        results[:templates] = Template.recommended.named_like(q)
+      end
+    end
+  end
+
+  def search_params
+    params.permit(:q, :type)
   end
 
 end
