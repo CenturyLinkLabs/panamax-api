27af9c104d174d86131e672d572d8f05ac2e5bce
Rupak Ganguly <rupakg@gmail.com>
2014-05-30 14:57:41 -0400

    Refactor panamax components code.

--- lib/panamax_agent/panamax/client/components.rb
+++ lib/panamax_agent/panamax/client/components.rb
@@ -6,35 +6,23 @@ module PanamaxAgent
         PANAMAX_COMPONENTS = %w(panamax-ui panamax-api)
 
         def list_components
-          components = [
-            {
-              'panamax-agent' => [
-                {
-                  'versions' => { PanamaxAgent::VERSION => '' }
-                }
-              ]
-            }
-          ]
-
+          components = {}
+          components['panamax-agent'] = { 'versions' => { PanamaxAgent::VERSION => '' } }
           PANAMAX_COMPONENTS.each do |component|
-            comp_metadata = []
-            comp_metadata << {
-              'versions' => registry_client.list_repository_tags(component)
-            }
-            components << { component => comp_metadata }
+            comp_metadata = { 'versions' => registry_client.list_repository_tags(component) }
+            components[component] = comp_metadata
           end
           components
         end
 
         def get_component(component)
-          comp = []
           if component == 'panamax-agent'
             comp_versions = { PanamaxAgent::VERSION => '' }
-            comp << { 'versions' => comp_versions }
+            comp = { 'versions' => comp_versions }
           else
             if PANAMAX_COMPONENTS.include?(component)
               comp_versions = registry_client.list_repository_tags(component)
-              comp << { 'versions' => comp_versions }
+              comp = { 'versions' => comp_versions }
             end
           end
           comp
