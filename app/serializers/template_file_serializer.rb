a4ab38154defc68d9d26a58ad30e4630562ad204
Brian DeHamer <brian@dehamer.com>
2014-06-16 14:03:11 -0700

    Support template serialization in YAML

--- app/serializers/template_file_serializer.rb
+++ app/serializers/template_file_serializer.rb
@@ -1,8 +1,11 @@
 class TemplateFileSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes :name, :description, :keywords, :recommended, :icon
+  attributes :name, :description, :keywords, :recommended, :icon, :documentation
 
   has_many :images, serializer: TemplateImageSerializer
 
+  def to_yaml
+    as_json.deep_stringify_keys.to_yaml
+  end
 end
