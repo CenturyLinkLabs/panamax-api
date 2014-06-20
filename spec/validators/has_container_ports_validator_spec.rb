c45b2fb1487ddf7b5fd4efd9accaff25cfc469e9
Mike Arnold <dharmamike@gmail.com>
2014-06-20 11:17:47 -0500

    testing port validators

---
+++ spec/validators/has_container_ports_validator_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe HasContainerPortsValidator do
+
+  subject{ described_class.new({ attributes: [:ports_attr] }) }
+
+  let(:model) { double('model') }
+
+  before do
+    model.stub_chain('errors', '[]').and_return({})
+  end
+
+  it 'returns no errors for ports with container ports' do
+    expect(model).to_not receive('errors')
+    subject.validate_each(model, 'ports_attr', [{ 'host_port' => 80, 'container_port' => 8080 },
+                                                { 'host_port' => 81, 'container_port' => 7070 }])
+  end
+
+  it 'should validate invalid address' do
+    expect(model.errors[]).to receive('<<').with("container port can't be blank").once
+    subject.validate_each(model, 'ports_attr', [{ 'host_port' => 80 }])
+  end
+
+end
---
+++ spec/validators/has_unique_ports_validator_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe HasUniquePortsValidator do
+
+  subject{ described_class.new({ attributes: [:ports_attr] }) }
+
+  let(:model) { double('model') }
+
+  before do
+    model.stub_chain('errors', '[]').and_return({})
+  end
+
+  it 'returns no errors for ports with when they are unique' do
+    expect(model).to_not receive('errors')
+    subject.validate_each(model, 'ports_attr', [{ 'host_port' => 80, 'container_port' => 8080 },
+                                                { 'host_port' => 81, 'container_port' => 7070 }])
+  end
+
+  it 'returns errors when there are duplicated ports' do
+    expect(model.errors[]).to receive('<<').with('host ports must be unique').once
+    subject.validate_each(model, 'ports_attr', [{ 'host_port' => 80 }, { 'host_port' => 80 }])
+  end
+
+end
