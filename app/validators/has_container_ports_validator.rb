75619fd10be621665862729af578f31a87937b9a
Brian DeHamer <brian@dehamer.com>
2014-06-23 15:28:53 -0700

    Alt impl for HasContainerPortsValidator

--- app/validators/has_container_ports_validator.rb
+++ app/validators/has_container_ports_validator.rb
@@ -1,13 +1,10 @@
 class HasContainerPortsValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return unless value.present?
-    catch(:done) do
-      value.each do |port|
-        unless port['container_port'].present?
-          record.errors[attribute] << "container port can't be blank"
-          throw :done
-        end
-      end
+    value.each do |port|
+      next if port['container_port'].present?
+      record.errors[attribute] << "container port can't be blank"
+      break
     end
   end
 end
--- spec/validators/has_container_ports_validator_spec.rb
+++ spec/validators/has_container_ports_validator_spec.rb
@@ -4,21 +4,42 @@ describe HasContainerPortsValidator do
 
   subject { described_class.new(attributes: [:ports_attr]) }
 
-  let(:model) { double('model') }
+  let(:record) { Image.new }
+  let(:attribute) { 'ports' }
 
-  before do
-    model.stub_chain('errors', '[]').and_return({})
+  context 'when the attribute is valid' do
+
+    let(:value) do
+      [
+        { 'host_port' => 80, 'container_port' => 8080 },
+        { 'host_port' => 81, 'container_port' => 7070 }
+      ]
+    end
+
+    it 'returns no errors for ports with container ports' do
+      subject.validate_each(record, attribute, value)
+      expect(record.errors).to be_empty
+    end
   end
 
-  it 'returns no errors for ports with container ports' do
-    expect(model).to_not receive('errors')
-    subject.validate_each(model, 'ports_attr', [{ 'host_port' => 80, 'container_port' => 8080 },
-                                                { 'host_port' => 81, 'container_port' => 7070 }])
-  end
+  context 'when the attribute is invalid' do
 
-  it 'should validate invalid address' do
-    expect(model.errors[]).to receive('<<').with("container port can't be blank").once
-    subject.validate_each(model, 'ports_attr', [{ 'host_port' => 80 }])
+    let(:value) do
+      [
+        { 'host_port' => 80 },
+        { 'host_port' => 81 }
+      ]
+    end
+
+    it 'should set an error on the record' do
+      subject.validate_each(record, attribute, value)
+      expect(record.errors).to_not be_empty
+    end
+
+    it 'should quit validating after the first error is detected' do
+      subject.validate_each(record, attribute, value)
+      expect(record.errors[attribute].size).to eq 1
+    end
   end
 
 end
