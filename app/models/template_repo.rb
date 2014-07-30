14a961df958c4ee1b1cddf0508b879e882cb23cf
Mike Arnold <dharmamike@gmail.com>
2014-07-30 16:58:57 -0500

    adding a post destroy callback to remove templates associated with the destroyed repo [finishes #75913588]
    Signed-off-by: Laura Frank <ljfrank@gmail.com>

--- app/models/template_repo.rb
+++ app/models/template_repo.rb
@@ -1,8 +1,15 @@
 class TemplateRepo < ActiveRecord::Base
 
+  after_destroy :purge_templates
+
   validates :name, presence: true, uniqueness: true
 
   def files
     GithubRepoFileEnumerator.new(name)
   end
+
+  def purge_templates
+    Template.destroy_all(source: self.name)
+  end
+
 end
--- spec/models/template_repo_spec.rb
+++ spec/models/template_repo_spec.rb
@@ -8,4 +8,11 @@ describe TemplateRepo do
   describe '#files' do
     its(:files) { should be_a TemplateRepo::GithubRepoFileEnumerator }
   end
+
+  describe 'after_destroy callback' do
+    it 'destroys all templates in the db sourced from that repo' do
+      template_repos(:repo1).destroy
+      expect(Template.where(source: template_repos(:repo1).name).count).to eq 0
+    end
+  end
 end
