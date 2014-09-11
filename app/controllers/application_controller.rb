562fc2b89dc8c7f0c6c4dedcc7e9c58d76bd7ccf
Alex Welch <me@alexwelch.com>
2014-09-11 16:30:35 -0600

    spike

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  http_basic_authenticate_with name: 'admin', password: 'password'
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
