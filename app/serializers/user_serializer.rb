f01c47679a78d087a6c5564dcef7d612e80dbb14
Alex Welch <me@alexwelch.com>
2014-10-15 16:17:17 -0600

    Upgrade ActiveModel Serializers. [finishes #80755262]
    Signed-off-by: Don Petersen <don@donpetersen.net>

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes :email, :repos, :github_access_token_present?, :github_username
+  attributes :email, :repos, :github_access_token_present, :github_username
 
-  def github_access_token_present?
+  def github_access_token_present
     object.github_access_token.present?
   end
 end
