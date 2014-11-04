8f66e4b993db36c66177da680d1231f598952a7a
Don Petersen <don@donpetersen.net>
2014-11-04 14:40:11 +0000

    Nest metadata within metadata refresh response.
    This accomplishes two things: it makes room for success/error
    information in our response, and makes this endpoint easier to use in
    ActiveResource. It's going to be hard to 'create' a refresh but return a
    metadata object. If the refresh object that is returned contains
    metadata, though, that jives better with ActiveResource.

--- app/controllers/remote_metadata_refreshes_controller.rb
+++ app/controllers/remote_metadata_refreshes_controller.rb
@@ -3,8 +3,11 @@ class RemoteMetadataRefreshesController < ApplicationController
 
   def create
     deployment_target = DeploymentTarget.find(params[:deployment_target_id])
+    metadata = deployment_target.refresh_metadata
+    serialized_metadata = DeploymentTargetMetadataSerializer.new(metadata)
+
     respond_with(
-      deployment_target.refresh_metadata,
+      { metadata: serialized_metadata },
       location: deployment_targets_path
     )
   end
