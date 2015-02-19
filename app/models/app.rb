bffce969092239ad6e31d7a8f38a1d8d5d4a76be
Brian DeHamer <brian@dehamer.com>
2015-02-19 15:31:27 -0800

    Submit services to fleet in sorted order
    [#88664650]

--- app/models/app.rb
+++ app/models/app.rb
@@ -6,16 +6,21 @@ class App < ActiveRecord::Base
 
   before_save :resolve_name_conflicts
 
+  def sorted_services
+    @sorted_services ||= ServiceSorter.sort(services)
+  end
+
   def run
-    services.each(&:submit)
-    services.each(&:start)
+    sorted_services.each(&:submit)
+    sorted_services.each(&:load)
+    sorted_services.each(&:start)
   end
 
   def restart
-    services.each(&:shutdown)
-    sleep(1)
-    services.each(&:submit)
-    services.each(&:start)
+    sorted_services.each(&:shutdown)
+    sorted_services.each(&:submit)
+    sorted_services.each(&:load)
+    sorted_services.each(&:start)
   end
 
   def add_service(params)
@@ -53,5 +58,41 @@ class App < ActiveRecord::Base
     sanitized_name = name.gsub('/', '_')
     count = App.where('name LIKE ?', "#{sanitized_name}%").count
     self.name = (count > 0) ? "#{sanitized_name}_#{count}" : sanitized_name
+  end
+
+  class ServiceSorter
+
+    def self.sort(services)
+      new(services).sort
+    end
+
+    def initialize(services)
+      @services = services
+      @unprocessed = Array.new(services)
+      @marked = Set.new
+      @sorted = []
+    end
+
+    def sort
+      @services.each { |service| visit(service) }
+      @sorted
+    end
+
+    private
+
+    def visit(service)
+      if @marked.include?(service)
+        raise 'circular link reference'
+      end
+
+      if @unprocessed.include?(service)
+        @marked.add(service)
+        service.links.each { |link| visit(link.linked_to_service) }
+        @marked.delete(service)
+
+        @unprocessed.delete(service)
+        @sorted << service
+      end
+    end
   end
 end
